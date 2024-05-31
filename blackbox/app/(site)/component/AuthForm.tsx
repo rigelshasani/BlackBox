206094ddad5539891350dd536a31a5554b4c542d
VrushankVaidhya <vaidhyavrushank@gmail.com>
2024-05-31 11:10:40 -0400

    login  Functionality With Single Sign On Feature Completed.

--- blackbox/app/(site)/component/AuthForm.tsx
+++ blackbox/app/(site)/component/AuthForm.tsx
@@ -6,6 +6,9 @@ import Button from "@/app/components/Button";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type variant = 'LOGIN' | 'REGISTER';
 
@@ -40,20 +43,46 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant == 'REGISTER') {
-      // Axios Register 
+       axios.post('/api/register', data)
+       .catch(() => toast.error('Something went wrong!'))
+       .finally(() => setIsLoading(false))
     }
 
-    if (variant == 'LOGIN') {
-      // NextAuth SingIn
-    }
+    if (variant =='LOGIN') {
 
-    const socialAction = (action: string) => {
-      setIsLoading(true);
-
-      // NextAuth Social Sign In 
-
-    }
+      signIn('credentials', {
+        ... data,
+        redirect: false
+      })
+      .then((callback) => {
+      if (callback?.error) {
+        toast.error('Invalid credentials');
+      }
+      
+      if (callback?.ok && !callback?.error) { 
+        toast.success('Logged in!')
+      }
+    })
+    .finally(() => setIsLoading(false));
   }
+}
+
+const socialAction = (action: string) => {
+  setIsLoading(true);
+
+  signIn(action, { redirect: false })
+  .then((callback) => {
+    if (callback?.error) {
+      toast.error('Invalid Credentials');
+    }
+
+    if (callback?.ok && !callback?.error) { 
+      toast.success ('Logged in!')
+    }
+  })
+  .finally(() => setIsLoading(false));
+}
+
   return (
     <div
       className="
@@ -167,7 +196,7 @@ const AuthForm = () => {
                   text-gray-500
                   ">
           <div>
-            {variant == 'LOGIN' ? 'New to Messanger?' : 'Already have an account?'}
+            {variant == 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
           </div>
           <div
             onClick={toggleVariant}
