8fcb2dc5ecba79a34bebad43a06596d087588552
Rig <rigels1304@gmail.com>
2024-06-03 00:50:32 +0200

    Changed picture

--- blackbox/app/(site)/page.tsx
+++ blackbox/app/(site)/page.tsx
@@ -2,22 +2,27 @@ import Image from "next/image";
 
 import AuthForm from "./component/AuthForm";
 
-//Homepage 
+//Homepage
 // TODO: Welcome Page with greetings
 
-
 export default function Home() {
-    return (
-        <div className=" flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-zinc-900">
+  return (
+    <div className=" flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-zinc-900">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md rounded-lg">
+        <Image
+          alt="logo"
+          height="150"
+          width="150"
+          className="mx-auto w-auto mt-6"
+          src="/images/logo.png"
+        />
 
-            <div className="sm:mx-auto sm:w-full sm:max-w-md bg-neutral-950 rounded-lg">
-                <Image alt="logo" height="80" width="80" className="mx-auto w-auto mt-6" src="/images/logo.png" />
-
-                <h2 className="mt-6 mb-6 text-center text-3xl font-bold tracking-tight text-white"> Sign into your account
-                </h2>
-            </div>
-            <AuthForm />
-
-        </div>
-    );
+        <h2 className="mt-6 mb-6 text-center text-3xl font-bold tracking-tight text-white">
+          {" "}
+          <i>Sign into your account</i>
+        </h2>
+      </div>
+      <AuthForm />
+    </div>
+  );
 }
