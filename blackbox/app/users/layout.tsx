a5237a214d50ab7cd6dec23746acd5e087f629b6
gaganjeetvaid <gaganjeetvaid@gmail.com>
2024-06-06 19:41:45 -0400

    Dash Board Login Created

---
+++ blackbox/app/users/layout.tsx
@@ -0,0 +1,18 @@
+//user layout after login(Dashboard)
+
+import Sidebar from "../components/sidebar/Sidebar";
+
+export default async function UsersLayout({
+    children
+}: {
+    children: React.ReactNode;
+
+}) {
+    return (
+      
+        <Sidebar>
+            <div className="h-full">{children}</div>
+        </Sidebar>
+    )
+
+}
